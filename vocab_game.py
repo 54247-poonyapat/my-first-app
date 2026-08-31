0f80f49d17856889abfda040fddfb61d61b65949
54247-poonyapat <54247@yupparaj.ac.th>
2026-08-31 14:00:58 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -101,4 +101,4 @@ if st.session_state.get("is_ended", False):
     show_result_dialog(ans1, ans2)
 
 st.divider()
-  st.write("นายปุญญพัฒน์ คำนา เลขที่ 8 ม.4/14")
+st.write("นายปุญญพัฒน์ คำนา เลขที่ 8 ม.4/14")
